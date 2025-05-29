8375c70ed76d5fb6f44afd35da69ffbb712762c7
vicky haldar <vickyh8810@gmail.com>
2025-05-29 19:24:22 +0530

    Initial commit

---
+++ index.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class GuessNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int myNumber = (int)(Math.random()*100);
+
+        int userNumber = 0;
+        do{
+            System.out.println("Guess My Number(1-100)");
+            userNumber = sc.nextInt();
+
+
+            if(myNumber == userNumber)
+            {
+                System.out.println("You're Correct");
+                break;
+            }
+            else if(myNumber > userNumber)
+            {
+                System.out.println("Your number is too Smaller");
+            }
+            else if(myNumber < userNumber)
+            {
+                System.out.println("Your number is too larger");
+            }
+        }
+        while(userNumber >= 0);
+        System.out.println("My number was : " + myNumber);
+    } 
+}
